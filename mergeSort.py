0f0ce1eb9a8da68e36609ac325bccae25a77ed20
Enojia <enojia.cl@gmail.com>
2016-01-13 23:18:41 +0100

    adding a functional version of mergeSort algo in python

--- mergeSort.py
+++ mergeSort.py
@@ -3,11 +3,11 @@ import vimpdb
 
 def merge_sort(array, p, r):
     if p<r:
-        q = (r+p)/2
+        q = (r+p)//2
         merge_sort(array, p, q)
         merge_sort(array, q+1, r)
         #vimpdb.set_trace()
-        return merge(array, p, q, r)
+        merge(array, p, q, r)
 
 def merge(array, p, q, r):
     n1 = q-p+1
@@ -16,35 +16,24 @@ def merge(array, p, q, r):
     left = []
     right = []
 
-    for i in range(0, n1):
-        left.insert(0,array[p+i])
+    for g in range(0, n1):
+        left.append(array[p+g])
 
-    for i in range(0, n2):
-        right.insert(0,array[q+i+1])
-
-    print left
-    print right
+    for h in range(0, n2):
+        right.append(array[q+h+1])
 
     i = 0
     j = 0
-    sortArray = []
 
-    for h in range(0, r-p+1):
-        sortArray.append(0)
-
-    print "sortArray"
-    print sortArray
-
-    for k in range(p, r):
-        if i<=q and left[i] <= right[j]:
+    for k in range(p, r+1):
+        if i<len(left) and (j==len(right) or left[i] <= right[j]):
             array[k]  = left[i]
             i+=1
-        elif j<=r:
+        else:
             array[k] = right[j]
             j+=1
-    return array
 
 
-array = [10, 23, 212, 23, 15, 13, 25, 36, 40]
-merge_sort(array, 0, len(array))
-
+array = [10, 23, 212 , 15, 13, 25, 36, 40, 10, 2, 4, 65]
+merge_sort(array, 0, len(array)-1)
+print array

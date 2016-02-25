ca3718599f665b22f97a1cab12068f7698b7e55a
enojia <enojia.cl@gmail.com>
2016-02-25 15:07:10 +0100

    caesar cypher encryptor

---
+++ encryptor.py
@@ -0,0 +1,22 @@
+#!/bash/bin/Python2
+
+alpha = "abcdefghijklmnopqrstuvwxyz"
+Alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def encryptor(key, message):
+    crypt_arr = []
+
+    for char in message:
+        if char in alpha:
+            crypt_arr.append(alpha[(alpha.index(char) + key) % 26])
+
+        elif char in Alpha:
+            crypt_arr.append(alpha[(alpha.index(char) + key) % 26])
+        
+        else:
+            crypt_arr.append(char)
+
+    return "".join(crypt_arr)
+
+crypt_message =  encryptor(2, "pythonay is the best one")
+print crypt_message
